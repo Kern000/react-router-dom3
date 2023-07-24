a089a19f761ad9a1a746ea5a7b357da92b6aad4a
unknown <holupkern@gmail.com>
2023-07-24 17:26:41 +0800

    first commit - mini practice 3 for react router dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Page1 from "./pages/page1";
+import Page2 from "./pages/page2";
+import Page3 from "./pages/page3";
+import "bootstrap/dist/css/bootstrap.min.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App(){
+  return(
+  <Router>
+    <nav className="navbar navbar-expand-md bg-light">
+      <ul className="navbar">
+        <li className="nav-item ms-4">
+          <Link to="/page1" className="nav-link"> Page 1 </Link>
+        </li>
+        <li className="nav-item ms-4">
+          <Link to="/page2" className="nav-link"> Page 2 </Link>
+        </li>
+        <li className="nav-item ms-4">
+          <Link to="/page3" className="nav-link"> Page 3 </Link>
+        </li>
+      </ul>
+    </nav>
+    <Routes>
+      <Route path="/page1" element={<Page1/>} />
+      <Route path="/page2" element={<Page2/>} />
+      <Route path="/page3" element={<Page3/>} />
+    </Routes>
+  </Router>
+  )
 }
-
-export default App;
