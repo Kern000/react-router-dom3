23cc8181b2579409efc89e0c531cdd4b29eb67d9
unknown <holupkern@gmail.com>
2023-07-25 15:41:37 +0800

    added useNavigate Hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link, BrowserRouter } from "react-router-dom";
 import Page1 from "./pages/page1";
 import Page2 from "./pages/page2";
 import Page3 from "./pages/page3";
 import "bootstrap/dist/css/bootstrap.min.css"
+import { useNavigate } from 'react-router-dom';
+
+// useNavigate is used for specifying which route to use
+// useNavigate needs to be wrapped by a router, in the root.render in index.js put <Router> </Router>
 
 export default function App(){
+  
+  const navigate = useNavigate();
+
+  function clickToNavigate(event){
+    console.log("event =>", event)
+    navigate(event.target.name)
+  }
+
   return(
-  <Router>
-    <nav className="navbar navbar-expand-md bg-light">
+    <React.Fragment>
+      <nav className="navbar navbar-expand-md bg-light">
       <ul className="navbar">
         <li className="nav-item ms-4">
-          <Link to="/page1" className="nav-link"> Page 1 </Link>
+          <Link to="/page1" name="/page1" className="nav-link" onClick={(evt) => clickToNavigate(evt)}> Page 1 </Link>
         </li>
         <li className="nav-item ms-4">
-          <Link to="/page2" className="nav-link"> Page 2 </Link>
+          <Link to="/page2" name="/page2" className="nav-link" onClick={(evt) => clickToNavigate(evt)}> Page 2 </Link>
         </li>
         <li className="nav-item ms-4">
-          <Link to="/page3" className="nav-link"> Page 3 </Link>
+          <Link to="/page3" name="/page3" className="nav-link" onClick={(evt) => clickToNavigate(evt)}> Page 3 </Link>
         </li>
       </ul>
     </nav>
@@ -26,6 +38,7 @@ export default function App(){
       <Route path="/page2" element={<Page2/>} />
       <Route path="/page3" element={<Page3/>} />
     </Routes>
-  </Router>
+    </React.Fragment>
+      
   )
 }
--- src/pages/page3.js
+++ src/pages/page3.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 export default function Page3(){
+    
     return(
         <div>
             <h1>Page 3</h1>
